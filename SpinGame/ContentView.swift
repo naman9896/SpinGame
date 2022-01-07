933829972fd52001d87f04188de3b6f9ffe95e12
Naman <namanmattu36166@gmail.com>
2022-01-08 00:50:15 +0530

    Initial Commit

---
+++ SpinGame/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  SpinGame
+//
+//  Created by Naman on 07/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var credits = 1000
+    @State var slot1 = 1
+    @State var slot2 = 1
+    @State var slot3 = 1
+    
+    var body: some View {
+        VStack(spacing: 20.0) {
+            
+            Spacer()
+            Text("SwiftUI Slots!").font(.largeTitle)
+            Spacer()
+            
+            Text("Credits: \(credits)")
+            
+            Spacer()
+            
+            HStack {
+                
+                Image("fruit\(slot1)").resizable().aspectRatio(contentMode: .fit)
+                Image("fruit\(slot2)").resizable().aspectRatio(contentMode: .fit)
+                Image("fruit\(slot3)").resizable().aspectRatio(contentMode: .fit)
+                
+            }
+            
+            Spacer()
+            
+            Button("Spin") {
+                
+                // Randomize the numbers
+                slot1 = Int.random(in: 1...3)
+                slot2 = Int.random(in: 1...3)
+                slot3 = Int.random(in: 1...3)
+                
+                // Update credits based on slot match or not
+                if slot1 == slot2 && slot2 == slot3 {
+                    // Match
+                    credits += 15
+                } else {
+                    // Not Match
+                    credits -= 5
+                }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .padding([.leading, .trailing], 40)
+            .foregroundColor(.white)
+            .background(Color(.systemPink))
+            .cornerRadius(25)
+            .font(.system(size: 18, weight: .bold, design: .default))
+            
+            
+            Spacer()
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpinGame/SpinGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinGameApp.swift
+//  SpinGame
+//
+//  Created by Naman on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SpinGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
